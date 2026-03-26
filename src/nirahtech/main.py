78337852374e1c702049157db4a418d1c69b2883
Nicolas Metivier <nicolas.a.metivier@gmail.com>
2026-03-26 11:10:29 +0100

    Add design patterns (factory, builder)

--- src/nirahtech/main.py
+++ src/nirahtech/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from .profile.application.domain.models import PlayerProfile
 from .profile.infrastructure.adapters.outputs.repository import JsonPersistenceAdapter
+from .profile.application.domain.models import WeaponFactory, Weapon
 
 
 def main():
@@ -10,3 +11,22 @@ def main():
 
     profile = PlayerProfile("test", "Test", "Human", "Warrior", 1, 0, [], [])
     db.create_player_profile(profile)
+
+    other_player = PlayerProfile.builder()\
+            .with_level(70)\
+            .with_race("Blood Elf")\
+            .with_classes("Paladin")\
+            .build()
+    
+    weapon_factory = WeaponFactory()
+    sword = weapon_factory.create_sword(
+        id="ws-01",
+        name="Glaive de Guerre d'Azzinoth",
+        min_damage=10,
+        max_damage=20,
+        attack_speed=1.2,
+        critical_chance=0.5,
+        critical_rate=2
+    )
+
+    sword.generate_damage()
--- src/nirahtech/profile/application/domain/models.py
+++ src/nirahtech/profile/application/domain/models.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Set
+from abc import ABC
 
 @dataclass
 class Stat:
@@ -11,7 +12,67 @@ class Bag:
     id: str
     items: List[str] = field(default_factory=list)
 
-@dataclass
+
+class PlayerProfileBuilder:
+    def __init__(self):
+        self.__id: str = None
+        self.__pseudo: str = "Player"
+        self.__race: str = "Human"
+        self.__classes: str = "Warrior"
+        self.__level: int = 1
+        self.__experience: int = 0
+        self.__stats: Set[Stat] = set() # {}
+        self.__inventory: List[Bag] = list() # []
+
+    def with_id(self, id: str) -> PlayerProfileBuilder:
+        self.__id = id
+        return self
+
+    def with_pseudo(self, pseudo: str) -> PlayerProfileBuilder:
+        self.__pseudo = pseudo
+        return self
+    
+    def with_race(self, race: str) -> PlayerProfileBuilder:
+        self.__race = race
+        return self
+    
+    def with_classes(self, classes: str) -> PlayerProfileBuilder:
+        self.__classes = classes
+        return self
+    
+    def with_level(self, level: int) -> PlayerProfileBuilder:
+        self.__level = level
+        return self
+    
+    def with_experience(self, experience: int) -> PlayerProfileBuilder:
+        self.__experience = experience
+        return self
+    
+    def with_stats(self, stats: Set[Stat]) -> PlayerProfileBuilder:
+        self.__stats = stats
+        return self
+    
+    def with_specific_stat(self, stat: Stat) -> PlayerProfileBuilder:
+        self.__stats.add(stat)
+        return self
+    
+    def with_inventory(self, inventory: List[Bag]) -> PlayerProfileBuilder:
+        self.__inventory = inventory
+        return self
+    
+    def build(self) -> PlayerProfile:
+        return PlayerProfile(
+            id=self.__id,
+            pseudo=self.__pseudo,
+            race=self.__race,
+            classes=self.__classes,
+            level=self.__level,
+            experience=self.__experience,
+            stats=self.__stats,
+            inventory=self.__inventory
+        )
+
+@dataclass(frozen=True)
 class PlayerProfile:
     id: str
     pseudo: str
@@ -21,3 +82,68 @@ class PlayerProfile:
     experience: int
     stats: List[Stat]
     inventory: List[Bag]
+
+    @staticmethod
+    def builder() -> PlayerProfileBuilder:
+        return PlayerProfileBuilder()
+
+
+class Weapon(ABC):
+    def __init__(self, id: str, name: str, min_damage: int, max_damage: int, critical_chance: float, critical_rate: float, attack_speed: float):
+        self.__id: str = id
+        self.__name: str = name
+        self.__min_damage: int = min_damage
+        self.__max_damage: int = max_damage
+        self.__critical_chance: float = critical_chance
+        self.__critical_rate: float = critical_rate
+        self.__attack_speed: float = attack_speed
+    
+    def generate_damage(self) -> float:
+        pass # A implémenter
+
+class Sword(Weapon):
+    def __init__(self, id: str, name: str, min_damage: int, max_damage: int, critical_chance: float, critical_rate: float, attack_speed: float):
+        super().__init__(
+            id=id,
+            name=name,
+            min_damage=min_damage,
+            max_damage=max_damage,
+            critical_chance=critical_chance,
+            critical_rate=critical_rate,
+            attack_speed=attack_speed
+        )
+
+class Bow(Weapon):
+    def __init__(self, id: str, name: str, min_damage: int, max_damage: int, critical_chance: float, critical_rate: float, attack_speed: float):
+        super().__init__(
+            id=id,
+            name=name,
+            min_damage=min_damage,
+            max_damage=max_damage,
+            critical_chance=critical_chance,
+            critical_rate=critical_rate,
+            attack_speed=attack_speed
+        )
+
+class WeaponFactory:
+    def create_sword(self, id: str, name: str, min_damage: int, max_damage: int, critical_chance: float, critical_rate: float, attack_speed: float) -> Sword:
+        return Sword(
+            id=id,
+            name=name,
+            min_damage=min_damage,
+            max_damage=max_damage,
+            critical_chance=critical_chance,
+            critical_rate=critical_rate,
+            attack_speed=attack_speed
+        )
+
+    def create_bow(self, id: str, name: str, min_damage: int, max_damage: int, critical_chance: float, critical_rate: float, attack_speed: float) -> Bow:
+        return Bow(
+            id=id,
+            name=name,
+            min_damage=min_damage,
+            max_damage=max_damage,
+            critical_chance=critical_chance,
+            critical_rate=critical_rate,
+            attack_speed=attack_speed
+        )
